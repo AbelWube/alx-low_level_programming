c1cd05f0ca4cddc076efd858f378b8a43d4e4341
Gafat PLC <abel.wube@aait.edu.et>
2022-04-07 12:34:33 -0700

    added files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+
 /**
-  * main - Entry Point
-  * include a standard i/o
-  *
-  * Returns: Always 0 (success)
-  */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
