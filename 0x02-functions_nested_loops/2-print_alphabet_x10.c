1aa6f95f8ed1c8d72352e58a645cd3ae17623633
Gafat PLC <abel.wube@aait.edu.et>
2022-04-12 14:27:44 -0700

    values

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,23 +2,20 @@
 
 /**
  * print_alphabet_x10 - prints lower case 10 times
- * main - entry point
+ * print_alphabet_x10 - entry point
  *
- * Returns: 0
+ * Return: 0
  */
 
-int print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
-	int j = 0;
-
-	while (j <= 9)
-	{
+	int j;
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (j = 0; j < 9; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
-		j++;
 	}
-	return (0);
 }
