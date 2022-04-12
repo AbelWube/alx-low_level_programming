e08f0a506c289f933a785d0f07d1005fc2e6c9b7
Gafat PLC <abel.wube@aait.edu.et>
2022-04-12 16:13:44 -0700

    modulus divided by 10

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - last digit
+ * @n: parameter
+ * Auth: Abel Wube
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
