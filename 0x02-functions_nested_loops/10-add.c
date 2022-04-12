ec2c4b9dbb4be54de2d21835e0a053df2dbc6e73
Gafat PLC <abel.wube@aait.edu.et>
2022-04-12 16:56:23 -0700

    adder

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @x: first parameter
+ * @y: second parameter
+ * Auth: Abel Wube
+ *
+ * Return: result
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
