af609e29f89210b009d35814f1e8656de689f7f3
Gafat PLC <abel.wube@aait.edu.et>
2022-04-12 16:03:06 -0700

    this way

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - turns the absolute value of the number
+ * @n: parameter
+ * Auth: Abel Wube
+ *
+ * Return: result
+ */
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+		return (n);
+	else
+		return (-1 * n);
+}
