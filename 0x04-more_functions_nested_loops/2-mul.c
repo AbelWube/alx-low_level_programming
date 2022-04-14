b5db2dfceb1ea98670936fa923b68252914f222c
Gafat PLC <abel.wube@aait.edu.et>
2022-04-14 12:24:23 -0700

    a function that multiples two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies a two integers
+ * @a: the first number to be multiplied
+ * @b: the second number to be multiplied
+ * Auth: Abel Wube
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,5 +5,5 @@
 
 int _isdigit(int c);
 int _isupper(int c);
-
+int mul(int a, int b);
 #endif
