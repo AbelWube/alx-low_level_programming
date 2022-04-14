1533078b639c0016b5777fbebd6993fe7970c0df
Gafat PLC <abel.wube@aait.edu.et>
2022-04-14 12:32:44 -0700

    funxtion that prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Auth: Abel Wube
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i);
+	_putchar("\n");
+}
