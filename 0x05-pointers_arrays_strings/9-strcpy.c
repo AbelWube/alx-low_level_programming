693bdafa0d73dc04918a339c2c2d9ffc8f715fd1
Gafat PLC <abel.wube@aait.edu.et>
2022-04-18 13:53:18 -0700

    string pointed by scr

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - copies string pointed by src
+ * @dest: parameter
+ * @src: parameter
+ * Auth: Abel Wube
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	return (dest);
+}
