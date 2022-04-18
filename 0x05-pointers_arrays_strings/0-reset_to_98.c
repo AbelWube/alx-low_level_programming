94f8260d46530373c97edfd1b1d8b71469ad1cad
Gafat PLC <abel.wube@aait.edu.et>
2022-04-18 12:00:33 -0700

    program that changes the value using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - updates the value it points to
+ * @n: parameter
+ * Auth: Abel Wube
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n);
+{
+	*p = 98;
+	return (0);
+}
