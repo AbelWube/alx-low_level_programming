8287852e524a4556e4a56cb7ee2217e8ad9e844f
Gafat PLC <abel.wube@aait.edu.et>
2022-04-19 01:18:22 -0700

    changed main files of putchar to _putchar

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-int putchar(char c);
+int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
