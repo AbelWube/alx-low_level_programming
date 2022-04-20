e52a1b4d3607990dbc8a89c7dc6208879e49a6a8
Gafat PLC <abel.wube@aait.edu.et>
2022-04-20 00:45:42 -0700

    a program that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_strcat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *Auth: Abel Wube
+ *Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char dest;
+	char src;
+	int l1, l2, i;
+
+	l1 = strlen(dest);
+	l2 = strlen(src);
+
+	for (i = 0; i <= l2; i++)
+		dest[l1 + 1] = src[i];
+	return (*dest);
+}
