af3f4e52fcec475bcb3a58359ccdf03e8d5b4949
Gafat PLC <abel.wube@aait.edu.et>
2022-04-20 05:09:32 -0700

    program that joins to strings with truncation operator n 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,19 +5,29 @@
  *_strcat - concatenates two strings
  *@dest: the first string
  *@src: the second string
+ *@n: Truncation value
  *Auth: Abel Wube
  *Return: pointer to dest
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src, int n)
 {
 	char dest;
 	char src;
-	int l1, l2, i;
+	int l1, l2, i, n;
 
 	l1 = strlen(dest);
 	l2 = strlen(src);
 
 	for (i = 0; i <= l2; i++)
-		dest[l1 + i] = src[i];
+	{
+		if (i == n)
+		{
+			break;
+		}
+		else
+		{
+			dest[l1 + i] = src[i];
+		}
+	}
 	return (*dest);
 }
