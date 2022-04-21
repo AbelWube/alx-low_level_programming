53b9a574b45c7a12e6b087b3029a395fdffda1b4
Gafat PLC <abel.wube@aait.edu.et>
2022-04-21 14:16:54 -0700

    infinte add

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 char *_strcat(char *dest, char *src);
 char *rot13(char *str);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
