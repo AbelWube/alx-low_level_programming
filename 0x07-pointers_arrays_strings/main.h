b8d7dcea357057a3bd79101286623c999d827ffc
Gafat PLC <abel.wube@aait.edu.et>
2022-04-25 13:39:20 -0700

    changed the prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_memset(char *s, char b, unsigned int n);
+void *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 #endif
