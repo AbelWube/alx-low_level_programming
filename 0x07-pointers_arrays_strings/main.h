f7fdfc039bb2077f7097d54d9de16ebad5ea5646
Gafat PLC <abel.wube@aait.edu.et>
2022-04-25 14:23:56 -0700

    added a file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <stdio.h>
 char *_strchr(char *s, char c);
+void set_string(char **s, char *to);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
