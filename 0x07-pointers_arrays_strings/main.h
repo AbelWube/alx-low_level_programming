3d076310da080a1ef8d8d4e21be0383beebc0e58
Gafat PLC <abel.wube@aait.edu.et>
2022-04-25 13:58:56 -0700

    added a line to main .h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-char *_memcpy(char *dest, char *src, unsigned int n);
+void *_memcpy(void *dest, const void *src, size_t n);
 void *_memset(void *s, int c, size_t n);
 int _putchar(char c);
 #endif
